c23dfa4d363a141a1ff0f7d8e7184e9e7ace8c56
phpdev32 <firewater2311@yahoo.com>
2014-01-09 21:16:15 -0500

    Add tentative top-level exception handler, and increment

--- IOJones/main.m
+++ IOJones/main.m
@@ -8,7 +8,23 @@
 
 #import <Cocoa/Cocoa.h>
 
+void handle_exception(NSException *exception) {
+    @try {
+    NSString *file = [NSString stringWithFormat:@"/tmp/%ld.plist", roundtol([[NSDate date] timeIntervalSince1970])];
+    NSMutableDictionary *d = [NSMutableDictionary dictionary];
+    if (exception.name) [d setObject:exception.name forKey:@"name"];
+    if (exception.reason) [d setObject:exception.reason forKey:@"reason"];
+    if (exception.userInfo) [d setObject:exception.userInfo forKey:@"userInfo"];
+    if (exception.callStackReturnAddresses) [d setObject:exception.callStackReturnAddresses forKey:@"callStackReturnAddresses"];
+    if (exception.callStackSymbols) [d setObject:exception.callStackSymbols forKey:@"callStackSymbols"];
+    [d writeToFile:file atomically:true];
+    NSRunAlertPanel(@"Uncaught Exception", @"An uncaught exception has occurred, and the program will now terminate. Please post the revealed file (may contain personal information), to %@", nil, nil, nil, @"http://sourceforge.net/projects/iojones/support");
+    [NSWorkspace.sharedWorkspace selectFile:file inFileViewerRootedAtPath:file];
+    } @catch (NSException *e) {}
+}
+
 int main(int argc, char *argv[])
 {
+    NSSetUncaughtExceptionHandler(&handle_exception);
     return NSApplicationMain(argc, (const char **)argv);
 }
